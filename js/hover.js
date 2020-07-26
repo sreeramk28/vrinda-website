875962e3acab1bec7c01bf3cfce01128caefae35
Sreeram K <sreeramfireball@gmail.com>
2020-07-27 00:59:22 +0530

    contact page additions

---
+++ js/hover.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+$(".class_footer_icon_image_social_media_L").hover(function(){
+          $(this).css("background-color", "#0077B5");
+          }, function(){
+          $(this).css("background-color", "#252525");
+        });
+        $(".class_footer_icon_image_social_media_T").hover(function(){
+          $(this).css("background-color", "#1DA1F2");
+          }, function(){
+          $(this).css("background-color", "#252525");
+        });
+        $(".class_footer_icon_image_social_media_F").hover(function(){
+          $(this).css("background-color", "#3B5998");
+          }, function(){
+          $(this).css("background-color", "#252525");
+        });
+        $(".class_footer_icon_image_social_media_W").hover(function(){
+          $(this).css("background-color", "#25D366");
+          }, function(){
+          $(this).css("background-color", "#252525");
+        });
+});
