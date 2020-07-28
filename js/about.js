39035b6294c2318891b8f8ffca8368ef60c54c60
Sreeram K <sreeramfireball@gmail.com>
2020-07-28 11:10:51 +0530

    about us almost over

--- js/about.js
+++ js/about.js
@@ -1,16 +1,52 @@
 $(document).ready(function(){
 	var iw = $(window).width();
-	if(iw < 750) {
-		$(".class_breaker").removeClass("col");
+	if(iw < 992) {
+		$(".col-rem").hide();
+	}
+	else {
+		$(".col-rem").show();
+	}
+	if(iw < 768) {
+		$(".gray-toggle").css({"height": "480px"});
+	}
+	else {
+		$(".gray-toggle").css({"height": "240px"});
+	}
+	if(iw < 576) {
+		//$("html").hide();
+		$(".bh-styling").css("font-size", "22px");
+		$(".banner-image").css("height", "180px");
+		$(".container-for-just-a-heading").css({"padding-top": "60px", "padding-bottom": "60px"});
+	}
+	else {
+		$(".bh-styling").css("font-size", "30px");
+		$(".banner-image").css("height", "300px");
+		$(".container-for-just-a-heading").css({"padding-top": "120px", "padding-bottom": "120px"});	
 	}
 	$(window).resize(function() {
-		var w = $(window).width();
-		if(w > 750) {
-			$(".class_breaker").addClass("col");
+		var iw = $(window).width();
+		if(iw < 992) {
+			$(".col-rem").hide();
 		}
-		else if(w < 750) {
-			$(".class_breaker").removeClass("col");
-
+		else {
+			$(".col-rem").show();
+		}
+		if(iw < 768) {
+			$(".gray-toggle").css({"height": "480px"});
+		}
+		else {
+			$(".gray-toggle").css({"height": "240px"});
+		}
+		if(iw < 576) {
+		//$("html").hide();
+			$(".bh-styling").css("font-size", "22px");
+			$(".banner-image").css("height", "180px");
+			$(".container-for-just-a-heading").css({"padding-top": "60px", "padding-bottom": "60px"});
+		}
+		else {
+			$(".bh-styling").css("font-size", "30px");
+			$(".banner-image").css("height", "300px");
+			$(".container-for-just-a-heading").css({"padding-top": "120px", "padding-bottom": "120px"});	
 		}
 	});
 
