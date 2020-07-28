0a6fe6f394a897a2e20fc8b402ea5108fcc1a96c
Sreeram K <sreeramfireball@gmail.com>
2020-07-28 18:46:00 +0530

    about us page centerings

--- js/about.js
+++ js/about.js
@@ -7,10 +7,18 @@ $(document).ready(function(){
 		$(".col-rem").show();
 	}
 	if(iw < 768) {
-		$(".gray-toggle").css({"height": "480px"});
+		$(".profile-selector").css({"height": "480px"});
+		$(".achi").addClass("text-center");
+		$(".achi-2").addClass("achi-table-center");
+		$(".achi-table-wrap").addClass("text-center");
+		$(".class_about_headingA").css("border-left", "0");
 	}
 	else {
-		$(".gray-toggle").css({"height": "240px"});
+		$(".profile-selector").css({"height": "240px"});
+		$(".achi").removeClass("text-center");
+		$(".achi-2").removeClass("achi-table-center");
+		$(".achi-table-wrap").removeClass("text-center");
+		$(".class_about_headingA").css("border-left", "3px solid #fff");
 	}
 	if(iw < 576) {
 		//$("html").hide();
@@ -23,6 +31,18 @@ $(document).ready(function(){
 		$(".banner-image").css("height", "300px");
 		$(".container-for-just-a-heading").css({"padding-top": "120px", "padding-bottom": "120px"});	
 	}
+	if(iw >= 768 && iw < 992) {
+		$(".profile-description").css("height", "170px");
+		$(".profile-description-2").css("height", "200px");
+	}
+	else if(iw < 768) {
+		$(".profile-description").css("height", "85px");
+		$(".profile-description-2").css("height", "120px");
+	}
+	else {
+		$(".profile-description").css("height", "120px");
+		$(".profile-description-2").css("height", "150px");
+	}
 	$(window).resize(function() {
 		var iw = $(window).width();
 		if(iw < 992) {
@@ -32,10 +52,18 @@ $(document).ready(function(){
 			$(".col-rem").show();
 		}
 		if(iw < 768) {
-			$(".gray-toggle").css({"height": "480px"});
+			$(".profile-selector").css({"height": "480px"});
+			$(".achi").addClass("text-center");
+			$(".achi-2").addClass("achi-table-center");
+			$(".achi-table-wrap").addClass("text-center");
+			$(".class_about_headingA").css("border-left", "0");
 		}
 		else {
-			$(".gray-toggle").css({"height": "240px"});
+			$(".profile-selector").css({"height": "240px"});
+			$(".achi").removeClass("text-center");
+			$(".achi-2").removeClass("achi-table-center");
+			$(".achi-table-wrap").removeClass("text-center");
+			$(".class_about_headingA").css("border-left", "3px solid #fff");
 		}
 		if(iw < 576) {
 		//$("html").hide();
@@ -47,6 +75,18 @@ $(document).ready(function(){
 			$(".bh-styling").css("font-size", "30px");
 			$(".banner-image").css("height", "300px");
 			$(".container-for-just-a-heading").css({"padding-top": "120px", "padding-bottom": "120px"});	
+		}
+		if(iw >= 768 && iw < 992) {
+			$(".profile-description").css("height", "170px");
+			$(".profile-description-2").css("height", "200px");
+		}
+		else if(iw < 768) {
+			$(".profile-description").css("height", "85px");
+			$(".profile-description-2").css("height", "120px");
+		}
+		else {
+			$(".profile-description").css("height", "120px");
+			$(".profile-description-2").css("height", "150px");
 		}
 	});
 
