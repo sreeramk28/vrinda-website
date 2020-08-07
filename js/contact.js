b119e5cd5af544f42f8e7f523b7b078b3c5830a6
Sreeram K <sreeramfireball@gmail.com>
2020-08-08 01:24:18 +0530

    contact us page changed

--- js/contact.js
+++ js/contact.js
@@ -1,4 +1,16 @@
 $(document).ready(function() {
+	function addBorders1245() {
+		$(".adr-1").addClass("col-bord");
+		$(".adr-2").addClass("col-bord");
+		$(".adr-4").addClass("col-bord");
+		$(".adr-5").addClass("col-bord");
+	}
+	function addBorders135() {
+		$(".adr-1").addClass("col-bord");
+		//$(".adr-2").addClass("col-bord");
+		$(".adr-3").addClass("col-bord");
+		$(".adr-5").addClass("col-bord");
+	}
 	function colBreaker() {
 		if($(".row-3").length > 0) return;
 		$(".col-bg").removeClass("col-lg");
@@ -10,6 +22,7 @@ $(document).ready(function() {
 		$(".row-3").append(six);
 		var three = $(".adr-3");
 		$(".row-2").children().first().after(three);
+		
 	}
 	function colJoiner() {
 		if($(".row-3").length == 0) return;
@@ -20,6 +33,7 @@ $(document).ready(function() {
 		var five = $(".adr-5"); var six = $(".adr-6");
 		$(".row-2").children().last().before(five); $(".row-2").children().last().before(six);
 		$(".row-3").remove();
+		
 	}
 	var w = $(window).width();
 	if(w >= 768 && w < 992) {
@@ -39,6 +53,17 @@ $(document).ready(function() {
 		$(".banner-image").css("height", "300px");
 		$(".container-for-just-a-heading").css({"padding-top": "120px", "padding-bottom": "120px"});	
 	}
+	if(w >= 992) {
+		$(".col-mgn").removeClass("col-bord");
+		addBorders1245();
+	}
+	else if(w >= 768) {
+		$(".col-mgn").removeClass("col-bord");
+		addBorders135();
+	}
+	else {
+		$(".col-mgn").removeClass("col-bord");
+	}
 	$(window).resize(function() {
 		var w = $(window).width();
 		if(w >= 768 && w < 992) {
@@ -57,6 +82,17 @@ $(document).ready(function() {
 			$(".bh-styling").css("font-size", "30px");
 			$(".banner-image").css("height", "300px");
 			$(".container-for-just-a-heading").css({"padding-top": "120px", "padding-bottom": "120px"});	
+		}
+		if(w >= 992) {
+			$(".col-mgn").removeClass("col-bord");
+			addBorders1245();
+		}
+		else if(w >= 768) {
+			$(".col-mgn").removeClass("col-bord");
+			addBorders135();
+		}
+		else {
+			$(".col-mgn").removeClass("col-bord");
 		}
 	});
 });
